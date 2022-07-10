797235815e191684f1e2d0599b7614743d89d833
Jerome Allen-Smith <jeromeallen99@gmail.com>
2022-07-10 15:15:19 -0400

    commit tree 52500db049cc7bfe90550f4d64eabaf9bda2a40f

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const messages = [];
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+        debugger;
+    }
+
+    return messages;
+}
+
+function countDown() {
+    let i=10;
+    while (i >= 0) {
+        console.log(i);
+        i--
+    }
+}
